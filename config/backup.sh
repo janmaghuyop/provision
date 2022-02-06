215723c3d94461cc897de3f78949b8ad80f5b4cd
janmaghuyop <janmaghuyop@pm.me>
2022-02-06 16:17:59 +0800

    add xfce4-keyboard-shortcuts

--- config/backup.sh
+++ config/backup.sh
@@ -1,2 +1,4 @@
-dconf dump /org/gnome/desktop/wm/keybindings/ > keyboard_bindings.conf
+dconf dump /org/gnome/desktop/wm/keybindings/ > gnome_keyboard_bindings.conf
+
+cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml xfce4_keyboard_shortcuts.xml
 
--- config/restore.sh
+++ config/restore.sh
@@ -1,1 +1,3 @@
-dconf load /org/gnome/desktop/wm/keybindings/ < keyboard_bindings.conf
+dconf load /org/gnome/desktop/wm/keybindings/ < gnome keyboard_bindings.conf
+
+cp xfce4_keyboard_shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
