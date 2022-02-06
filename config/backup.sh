1fd9861f127017515d5c3e7ad599829ea3e6372f
janmaghuyop <janmaghuyop@pm.me>
2022-02-06 16:42:10 +0800

    fix

--- config/backup.sh
+++ config/backup.sh
@@ -1,4 +1,4 @@
 dconf dump /org/gnome/desktop/wm/keybindings/ > gnome_keyboard_bindings.conf
 
-cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml xfce4_keyboard_shortcuts.xml
+cp ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml xfce4-keyboard-shortcuts.xml
 
--- config/restore.sh
+++ config/restore.sh
@@ -1,3 +1,3 @@
 dconf load /org/gnome/desktop/wm/keybindings/ < gnome keyboard_bindings.conf
 
-cp xfce4_keyboard_shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+cp xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
